a7b816673c331a54bf35c1ee5e3a164cd7b3d87e
Victorsmith <victorsmith@Victorsmiths-MacBook-Air.local>
2022-03-04 09:25:27 -0500

    commit 47 permafrost

--- src/components/index/encabezado.js
+++ src/components/index/encabezado.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header(){
     return(
-        <div className='image hight-portada' >
+        <div className='image' >
             <div className='flex flex-wrap flex-col lg:justify-center lg:h-screen py-10 md:py-20 lg:py-0 px-4 md:px-10 lg:px-10'>
                 <div className='flex flex-col items-center w-full md:max-w-xl lg:max-w-2xl mt-48 md:mt-56 lg:mt-56 mx-auto'>
                     <h1 className='font-bold text-white text-base lg:text-2xl'>PROYECTO</h1>
